9ba68e3cf4b9a647bde4670d380268cc575b27a7
Hass <hassmroueh@gmail.com>
2024-09-22 00:06:59 -0400

    state lab complete

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
+
+
 
 function Item({ name, category }) {
+  const [isAdded, setAddedToCart] = useState(false)
+
+ function handleClick(){
+  setAddedToCart((isAdded)=> !isAdded)
+ }
+const liClass = isAdded ? "in-cart" : ""
+
+const cartClass = isAdded ? "remove" : "add"
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className={cartClass}>{isAdded ? "Remove From Cart" : "Add to Cart" }</button>
     </li>
   );
 }
